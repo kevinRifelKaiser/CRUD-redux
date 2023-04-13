04872785dcea088d8e96fe827d38141462e81721
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-04-14 00:22:20 +0100

    [feature] - install and configure tremor. Create CardList component with the firs data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+    // Path to the tremor module
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
